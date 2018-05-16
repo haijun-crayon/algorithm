679e4b366593c7b37986afb655394bd85bc14f06
haijun-crayon <senierbattery@sina.com>
2018-05-16 18:39:23 +0800

    Create quickSort.java

---
+++ quickSort.java
@@ -0,0 +1,26 @@
+    public int[] kSort(int[] nums) {
+        kuiSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void kuiSort(int[] nums, int i, int j) {
+        int left = i;
+        int right = j;
+        if (i >= j) return;
+        while (i < j) {
+            while (nums[j] >= nums[i] && i < j) {
+                j--;
+            }
+            if (i < j) {
+                swap(nums, i, j);
+            }
+            while (nums[i] <= nums[j] && i < j) {
+                i++;
+            }
+            if (i < j) {
+                swap(nums, i, j);
+            }
+        }
+        kuiSort(nums, left, i - 1);
+        kuiSort(nums, i + 1, right);
+    }
